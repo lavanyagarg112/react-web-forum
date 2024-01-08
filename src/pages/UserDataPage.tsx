25867ba9ac211f635e8e4fbe26009d8b57455520
Lavanya Garg <142018717+lavanyagarg112@users.noreply.github.com>
2024-01-08 21:55:52 +0800

    user data not logged in message added

--- src/pages/UserDataPage.tsx
+++ src/pages/UserDataPage.tsx
@@ -3,6 +3,7 @@ import UserDataForm from '../components/UserDataForm'
 import LogoutButton from '../components/LogoutButton'
 
 import { useAuth } from '../store/auth-context'
+import { Link } from 'react-router-dom'
 
 const UserDataPage = () => {
     const { isLoggedIn } = useAuth();
@@ -10,6 +11,7 @@ const UserDataPage = () => {
     <div>
       {isLoggedIn && <UserDataForm />}
       {isLoggedIn && <LogoutButton />}
+      {!isLoggedIn && <p> Please <Link to="/login">log in</Link> to view profile </p>}
     </div>
   )
 }

f92f620cda2617cad45c48da3e220fff701cf726
Lavanya Garg <142018717+lavanyagarg112@users.noreply.github.com>
2024-01-21 17:35:58 +0800

    hamburger

--- src/components/layouts/MainNavigation.tsx
+++ src/components/layouts/MainNavigation.tsx
@@ -1,58 +1,41 @@
-import React from 'react'
-import {Link} from 'react-router-dom';
-import { useContext } from 'react';
-
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import classes from './MainNavigation.module.css';
 import { useAuth } from '../../store/auth-context';
 
-
 const MainNavigation = () => {
-const auth = useAuth();
-let message = ""
-let link = "/"
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
+  const auth = useAuth();
+  let message = "Sign Up";
+  let link = "/signup";
 
-  if (!auth || !auth.isLoggedIn) {
-    message = "Sign Up"
-    link = "/signup"
-    console.log('not signed in')
-  } else {
-    message = "My Profile"
-    link = "/user-data"
+  if (auth && auth.isLoggedIn) {
+    message = "My Profile";
+    link = "/user-data";
   }
 
+  const toggleMenuHandler = () => {
+    setMenuIsOpen(prevState => !prevState);
+  };
+
   return (
-    <header className={classes.header}>
-            <div className={classes.logo}><Link to="/">Thread Talk</Link></div>
-            <nav>
-                <ul>
-                    {/* <li><a href="">Some Link</a></li> 
-                    dont want <a> cause extra request */}
+    <header className={`${classes.header} ${menuIsOpen ? 'menu-open' : ''}`}>
+      <div className={classes.logo}><Link to="/">Thread Talk</Link></div>
+      <div className={classes.hamburger} onClick={toggleMenuHandler}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <nav>
+        <ul>
+          <li><Link to="/newpost" onClick={toggleMenuHandler}>Add New Post</Link></li>
+          <li><Link to="/favourites" onClick={toggleMenuHandler}>My Favourites</Link></li>
+          <li><Link to="/Categories" onClick={toggleMenuHandler}>Categories</Link></li>
+          <li><Link to={link} onClick={toggleMenuHandler}>{message}</Link></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
-
-                    <li>
-                        <Link to="/newpost">Add New Post</Link>
-                    </li>
-
-                    <li>
-                        <Link to="/favourites">My Favourites 
-                        </Link>
-                    </li>
-
-                    <li>
-                        <Link to="/Categories">Categories</Link>
-                    </li>
-
-                    <li>
-                        <Link to={link}>
-                            {message}
-                            
-                        </Link>
-                    </li>
-
-                </ul>
-            </nav>
-        </header>
-  )
-}
-
-export default MainNavigation
+export default MainNavigation;
